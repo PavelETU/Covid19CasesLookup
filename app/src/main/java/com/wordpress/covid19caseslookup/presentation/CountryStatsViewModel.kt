63445e3b27d22983d1a9d6bece18cd70ebf79129
PavelETU <pavelsuvit@gmail.com>
2020-10-11 14:27:22 +0100

    Expose LiveData backed by MutableLiveData

--- app/src/main/java/com/wordpress/covid19caseslookup/presentation/CountryStatsViewModel.kt
+++ app/src/main/java/com/wordpress/covid19caseslookup/presentation/CountryStatsViewModel.kt
@@ -2,6 +2,7 @@ package com.wordpress.covid19caseslookup.presentation
 
 import android.content.Context
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,9 +13,12 @@ import kotlinx.coroutines.launch
 
 class CountryStatsViewModel @ViewModelInject constructor(var lookupRepo: LookupRepo,
                                                          @ApplicationContext private val context: Context) : ViewModel() {
-    val countryStats = MutableLiveData<List<CountryStats>>()
-    val showError = MutableLiveData<Boolean>()
-    val loading = MutableLiveData<Boolean>()
+    private val _countryStats = MutableLiveData<List<CountryStats>>()
+    val countryStats: LiveData<List<CountryStats>> = _countryStats
+    private val _showError = MutableLiveData<Boolean>()
+    val showError: LiveData<Boolean> = _showError
+    private val _loading = MutableLiveData<Boolean>()
+    val loading: LiveData<Boolean> = _loading
     private var slug: String? = null
     fun onSlugObtained(slug: String) {
         if (this.slug != slug) {
@@ -28,13 +32,13 @@ class CountryStatsViewModel @ViewModelInject constructor(var lookupRepo: LookupR
     }
 
     private fun loadStats() {
-        showError.value = false
-        loading.value = true
+        _showError.value = false
+        _loading.value = true
         viewModelScope.launch {
             val stats = lookupRepo.getCountrySummary(slug!!)
-            loading.value = false
-            if (stats.isEmpty()) showError.value = true
-            else this@CountryStatsViewModel.countryStats.value = stats
+            _loading.value = false
+            if (stats.isEmpty()) _showError.value = true
+            else this@CountryStatsViewModel._countryStats.value = stats
         }
     }
 }
