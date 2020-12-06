465dd6fbd9f16bab041b60c4af66d051de2d1e06
PavelETU <pavelsuvit@gmail.com>
2020-12-06 16:23:12 +0000

    Remove deprecated ActivityTestRule in favor of ActivityScenario

--- app/src/androidTest/java/com/wordpress/covid19caseslookup/CasesLookupRules.kt
+++ app/src/androidTest/java/com/wordpress/covid19caseslookup/CasesLookupRules.kt
@@ -1,6 +1,6 @@
 package com.wordpress.covid19caseslookup
 
-import androidx.test.rule.ActivityTestRule
+import androidx.test.core.app.ActivityScenario
 import androidx.test.rule.GrantPermissionRule
 import com.wordpress.covid19caseslookup.data.LookupRepo
 import com.wordpress.covid19caseslookup.presentation.MainActivity
@@ -12,19 +12,21 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.rules.RuleChain
+import org.junit.rules.TestRule
 import javax.inject.Singleton
 
 open class CasesLookupRules {
     private val hiltAndroidRule = HiltAndroidRule(this)
 
     @get:Rule
-    val hiltEmulatorTestRule = RuleChain.outerRule(hiltAndroidRule)
+    val hiltEmulatorTestRule: TestRule = RuleChain.outerRule(hiltAndroidRule)
         .around(GrantPermissionRule.grant(android.Manifest.permission.ACCESS_COARSE_LOCATION))
-        .around(ActivityTestRule(MainActivity::class.java))
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
 
     @Before
     fun setUp() {
         hiltAndroidRule.inject()
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
     }
 
     @Module
