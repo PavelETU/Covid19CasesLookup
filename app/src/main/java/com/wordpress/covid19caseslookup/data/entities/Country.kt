a2913eb607763070d459c85ce1fed567d82270e3
PavelETU <pavelsuvit@gmail.com>
2021-03-06 13:20:31 +0000

    Add room entities

--- app/src/main/java/com/wordpress/covid19caseslookup/data/entities/Country.kt
+++ app/src/main/java/com/wordpress/covid19caseslookup/data/entities/Country.kt
@@ -1,7 +1,11 @@
 package com.wordpress.covid19caseslookup.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class Country(@SerializedName("Country") val country: String,
+@Entity
+data class Country(@PrimaryKey(autoGenerate = true) val id: Int = 0,
+                   @SerializedName("Country") val country: String,
                    @SerializedName("Slug") val slug: String,
                    @SerializedName("ISO2") val iso: String)
--- app/src/main/java/com/wordpress/covid19caseslookup/data/entities/CountryStats.kt
+++ app/src/main/java/com/wordpress/covid19caseslookup/data/entities/CountryStats.kt
@@ -1,8 +1,12 @@
 package com.wordpress.covid19caseslookup.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-data class CountryStats(@SerializedName("Confirmed") val confirmed: Int,
+@Entity
+data class CountryStats(@PrimaryKey(autoGenerate = true) val id: Int = 0,
+                        @SerializedName("Confirmed") val confirmed: Int,
                         @SerializedName("Deaths") val deaths: Int,
                         @SerializedName("Recovered") val recovered: Int,
                         @SerializedName("Date") val date: String)
